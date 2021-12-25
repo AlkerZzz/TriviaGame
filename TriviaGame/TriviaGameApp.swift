a3aaae3fad89c387daba3eee09911cb370790b6d
AlkerZzz <alireza.s.shakeri@gmail.com>
2021-12-25 17:39:01 +1030

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Alireza Shakeri on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
